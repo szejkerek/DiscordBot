79c8850b9bf49bb026db3c19cbeebbf8475aafe4
szejkerek <bartekk.gordon@gmail.com>
2021-04-29 00:19:10 +0200

    RUBY BOT

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # discord-bot
+#https://discord.com/oauth2/authorize?client_id=832580049286463548&scope=bot
---
+++ discord-bot-v2/main.js
@@ -0,0 +1,62 @@
+const Discord = require('discord.js');
+const client = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
+ 
+const prefix = '$';
+ 
+const fs = require('fs');
+ 
+client.commands = new Discord.Collection();
+ 
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for(const file of commandFiles){
+    const command = require(`./commands/${file}`);
+ 
+    client.commands.set(command.name, command);
+}
+ 
+ 
+client.once('ready', () => {
+    console.log('BOT IS ONLINE!!!');
+});
+ 
+client.on('message', message =>{
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+ 
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+ 
+    if(command === 'ping'){
+        client.commands.get('ping').execute(message, args);
+    } 
+});
+
+
+client.on('messageReactionAdd', async (reaction, user) => {
+	if (reaction.partial) {
+		try {
+			await reaction.fetch();
+		} catch (error) {
+			console.error('Something went wrong when fetching the message: ', error);
+			return;
+		}
+	}
+	if(reaction.message.author.username === "Bartuś" && reaction.message.author.tag === "Bartuś#8551")
+    {
+        if(reaction.emoji.name === '5Head')
+            {
+                reaction.message.reactions.cache.get('837019465212100658').remove().catch(error => console.error('Failed to remove reactions: ', error));
+
+            }
+    }
+
+});
+
+
+client.on('message', message =>{
+
+    if(message.author.bot) return;
+
+    
+});
+ 
+client.login('ODM2OTk5MTMzMzE4MzQ4ODEw.YImKMA.LrH22g_2PlnOfUeLw7ssFrhJWEM');
