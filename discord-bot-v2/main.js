663a068fe5a39ce31585a224069f5197976120d4
szejkerek <bartekk.gordon@gmail.com>
2021-04-29 00:53:52 +0200

    TOKEN CHANGES

--- discord-bot-v2/main.js
+++ discord-bot-v2/main.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
- 
+require('dotenv').config();
 const prefix = '$';
  
 const fs = require('fs');
@@ -59,4 +59,4 @@ client.on('message', message =>{
     
 });
  
-client.login('ODM2OTk5MTMzMzE4MzQ4ODEw.YImKMA.LrH22g_2PlnOfUeLw7ssFrhJWEM');
+client.login(process.env.TOKEN);
